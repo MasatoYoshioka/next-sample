45ad9bdbe88e8c08fa82d7d3eae9efddb3655af5
yoshioka <yoshioka0627@gmail.com>
2020-11-27 19:09:13 +0900

    changed

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
+          hogehogehoohgoe
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
